2420d0f2ff4e9c0fafefb183aad5748badab6365
Nicholas Merolle <nmerolle11@hotmail.com>
2020-10-23 06:06:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,5 +5,15 @@ class Student < User
   def initialize
     @knowledge = []
   end
-
+  
+  def learn(lesson)
+    @knowledge << lesson
+  end
+  
+  def knowledge
+    @knowledge
+  end
+        
+    
+    
 end
